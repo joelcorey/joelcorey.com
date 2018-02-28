a989622c41738d6c91435184f4dba125e320e6e2
Joel Corey <joelcorey@fastmail.com>
2018-02-28 10:26:06 -0600

    first commit

---
+++ inc/conf.php
@@ -0,0 +1,5 @@
+<?php
+
+$key = 'key-91cd8c23e250c2289d97e2e465a9ec9b';
+
+$domain = 'joelcorey.com';
---
+++ inc/mail.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'conf.php';
+use Mailgun\Mailgun;
+
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$mgClient = new Mailgun($key);
+
+$result = $mgClient->sendMessage($domain, array(
+    'from'    => $email,
+    'to'      => $to,
+    'subject' => $subject,
+    'text'    => $message
+));
+
+header('Location: index.html');
